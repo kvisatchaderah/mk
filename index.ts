a227f305142f1e1ac235ee855b387c7afd9ed2eb
kvisatchaderah <kvisatchaderah@gmail.com>
2023-02-28 23:01:43 +0300

    -

---
+++ index.ts
@@ -0,0 +1,15 @@
+type Enums = '1' | '3' | '2'
+
+function setfd1(g: Enums): Enums {
+  return g
+}
+
+setfd1('3')
+
+type Enums1 = '4' | '5' | 2
+
+function setfd(g: Enums1): Enums1 {
+  return g
+}
+
+setfd('//')
